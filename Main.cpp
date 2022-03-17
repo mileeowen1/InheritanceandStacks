17c2d41cd1106ae95d33c4058397b2fe19f0af4a
Milee Owen <101599501+mileeowen1@users.noreply.github.com>
2022-03-17 10:37:59 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Milee Owen
+Class: CPSC 122, Spring 2020
+Date: April 12, 2020
+Programming Assignment: PA7
+Description: Create a program that solves two tasks: 1) Report gross salary amounts and benefits for employees at your company, 2) Convert INFIX expressions to POSTFIX expressions and then evaluate those POSTFIX expressions using stacks.
+Notes: Main cpp file
+*/
+#include "PA7.h"
+
+int main() {
+	// TODO: run task 1
+	runTask1();
+	// TODO: run task 2
+	runTask2();
+	return 0;
+}
